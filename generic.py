88c2469e2b964ee1bcefec3434d2ea2ac0f59eab
Stefano Cortinovis <stefanocortinovis17@gmail.com>
2019-11-24 09:52:46 +0100

    Create utils package for modules

--- generic.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-def import_datasets(path=Path('./data/ml-latest-small'), ratings_csv='ratings.csv', movies_csv='movies.csv'):
-    ratings = pd.read_csv(path/ratings_csv)
-    movies = pd.read_csv(path/movies_csv)
-    return ratings, movies
-    
